b5710b5152a4c6fe8d56c7f225412086a9e3ffe0
TriTran <thanhtritran8@gmail.com>
2024-08-21 00:15:06 +0700

    initial commit

---
+++ frontend/src/components/ProductDescription.jsx
@@ -0,0 +1,35 @@
+const ProductDescription = () => {
+  return (
+    <div className="mt-20">
+      <div className="flex gap-3 mb-4">
+        <button className="btn_dark_rounded !rounded-none !text-sx !py-[6px] w-36">
+          Description
+        </button>
+        <button className="btn_dark_outline !rounded-none !text-sx !py-[6px] w-36">
+          Care Guide
+        </button>
+        <button className="btn_dark_outline !rounded-none !text-sx !py-[6px] w-36">
+          Description
+        </button>
+      </div>
+      <div className="flex flex-col pb-16">
+        <p className="text-sm">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente rem
+          quo tenetur dolor in maxime iste assumenda maiores recusandae officiis
+          accusamus obcaecati consequuntur odio nostrum atque laudantium amet,
+          quod dolore deleniti! Alias perferendis qui voluptas in dolorum
+          voluptatem provident quod delectus modi, iste assumenda excepturi
+          temporibus veniam consequuntur repellat, minus recusandae optio.
+          Eaque, quas nemo.
+        </p>
+        <p className="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in
+          deleniti asperiores necessitatibus! Harum, eaque! Soluta sit eveniet
+          blanditiis, iusto est quaerat!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDescription;
